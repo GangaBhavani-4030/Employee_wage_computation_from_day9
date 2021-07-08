2afb61bbed5e159bcc43c53102b0283799a5f978
GangaBhavani-4030 <gangabhavani.4030@gmail.com>
2021-07-08 12:47:26 +0530

    to get working hours using function

--- Day9_Employee_wage_computation.sh
+++ Day9_Employee_wage_computation.sh
@@ -1,21 +1,24 @@
-#Calculate Wages till a condition of total working hours or days is reached for a month
-per_hr_wage=20
-max_hrs=100
-max_days=20
-total_working_hours=0
-total_working_days=0
-while(( total_working_hours < max_hrs && total_working_Days < max_days ))
-do
-	(( total_working_days++ ))
-	employee_attendace=$(( RANDOM % 3 ))
-	case $employee_attendace in
-		0) echo " employee is absent"
-		   no.of.days=0 ;;
-		1) echo "employee full time present"
-		   no.of.days=8 ;;
-		2) echo"employee part time present"
-		   no.of.days=4 ;;
+#function code
+get_workhours()
+{
+	employee_attendace=$1
+	case $employee_attendace 
+		0)num_hours_perday=0 ;;
+		1)num_hours_perday=8 ;;
+		2)num_hours_per_day=4 ;;
 	esac
+echo "num_hours_perday"
+}
+perhour_wage=20
+maximumhours-100
+maximumdays=20
+workinghours_intotal=0
+workingdays_intotal=0
+while(( workinghours_intotal < maximumhours && workingdays_intotal < maximumdays ))
+do
+	(( workingdays_intotal++ ))
+	employee_attendance=$(( RANDOM % 3 ))
+	num_hours_per_day=$(( get_workhours$employeeattendance))
+	(( workinghours_intotal += num_hours_perday ))
 done
-totalsalary=$(( total_working_hours * per_hr_wage ))
-echo "total salary = $totalsalary"
+
