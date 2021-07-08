208ff1261cfcf573f5a0fe8f803a901fcf5c4919
GangaBhavani-4030 <gangabhavani.4030@gmail.com>
2021-07-08 12:57:53 +0530

    total wage along with daily wage using sorting

--- Day9_Employee_wage_computation.sh
+++ Day9_Employee_wage_computation.sh
@@ -9,16 +9,23 @@ get_workhours()
 	esac
 echo "num_hours_perday"
 }
+declare -A wage
 perhour_wage=20
 maximumhours-100
 maximumdays=20
 workinghours_intotal=0
 workingdays_intotal=0
+totalwage=0
+echo "total wage along with daily wage:"
 while(( workinghours_intotal < maximumhours && workingdays_intotal < maximumdays ))
 do
 	(( workingdays_intotal++ ))
 	employee_attendance=$(( RANDOM % 3 ))
 	num_hours_per_day=$(( get_workhours$employeeattendance))
 	(( workinghours_intotal += num_hours_perday ))
+	dailywage=$(( num_hours_perday * perhour_wage ))
+	(( totalwage += dailywage ))
+	wage[$totalwage]=$dailywage
+	echo "$totalwage       along with $dailywage "
 done
 
